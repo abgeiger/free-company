98ed00060629e7ee805af73f8bb1aa9f2d76a1e4
abgeiger <30698092+abgeiger@users.noreply.github.com>
2018-01-04 14:50:15 -0600

    newGame() adds a new row to the game table and gets regiment templates

--- server/routes/game.router.js
+++ server/routes/game.router.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var pool = require('../modules/pool');
 
 router.post('/new', function (req, res) {
+
+    // add new game
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
         console.log('error', errorConnectingToDatabase);
@@ -17,51 +19,50 @@ router.post('/new', function (req, res) {
                 console.log('error', errorMakingDatabaseQuery);
                 res.sendStatus(500);
                 } else {
-                    var gameId = result.rows.data[0].id;
-                    res.sendStatus(200);
-                    // // get regiment templates
-                    // pool.connect(function (errorConnectingToDatabase, client, done) {
-                    //     if (errorConnectingToDatabase) {
-                    //     console.log('error', errorConnectingToDatabase);
-                    //     res.sendStatus(500);
-                    //     } else {
-                    //         var gameId = req.query.id;
-                    //         client.query(`SELECT * FROM regiment_template
-                    //                     ORDER BY id;`, [userId], function (errorMakingDatabaseQuery, result) {
-                    //             done();
-                    //             if (errorMakingDatabaseQuery) {
-                    //             console.log('error', errorMakingDatabaseQuery);
-                    //             res.sendStatus(500);
-                    //             } else {
-                    //                 res.send(result.rows);
+                    var gameId = result.rows[0].id;
+
+                    // get regiment templates
+                    pool.connect(function (errorConnectingToDatabase, client, done) {
+                        if (errorConnectingToDatabase) {
+                        console.log('error', errorConnectingToDatabase);
+                        res.sendStatus(500);
+                        } else {
+                            client.query(`SELECT * FROM regiment_template
+                                        ORDER BY id;`, function (errorMakingDatabaseQuery, result) {
+                                done();
+                                if (errorMakingDatabaseQuery) {
+                                console.log('error', errorMakingDatabaseQuery);
+                                res.sendStatus(500);
+                                } else {
+                                    res.send(result.rows);
                                     
 
-                    //                 // post new regiments
-                    //                 // pool.connect(function (errorConnectingToDatabase, client, done) {
-                    //                 //     if (errorConnectingToDatabase) {
-                    //                 //     console.log('error', errorConnectingToDatabase);
-                    //                 //     res.sendStatus(500);
-                    //                 //     } else {
-                    //                 //         var gameId = req.query.id;
-                    //                 //         client.query(`;`, [userId], function (errorMakingDatabaseQuery, result) {
-                    //                 //             done();
-                    //                 //             if (errorMakingDatabaseQuery) {
-                    //                 //             console.log('error', errorMakingDatabaseQuery);
-                    //                 //             res.sendStatus(500);
-                    //                 //             } else {
-                    //                 //             res.send(result.rows);
-                    //                 //             }
-                    //                 //         });
-                    //                 //     }
-                    //                 // }); // end post new regiments
-                    //             }
-                    //         });
-                    //     }
-                    // }); // end get regiment templates
+                                    // post new regiments
+                                    // pool.connect(function (errorConnectingToDatabase, client, done) {
+                                    //     if (errorConnectingToDatabase) {
+                                    //     console.log('error', errorConnectingToDatabase);
+                                    //     res.sendStatus(500);
+                                    //     } else {
+                                    //         var gameId = req.query.id;
+                                    //         client.query(`;`, [userId], function (errorMakingDatabaseQuery, result) {
+                                    //             done();
+                                    //             if (errorMakingDatabaseQuery) {
+                                    //             console.log('error', errorMakingDatabaseQuery);
+                                    //             res.sendStatus(500);
+                                    //             } else {
+                                    //             res.send(result.rows);
+                                    //             }
+                                    //         });
+                                    //     }
+                                    // }); // end post new regiments
+                                }
+                            });
+                        }
+                    }); // end get regiment templates
                 }
             });
         }
-    });
+    }); // end add new game
 });
 
 module.exports = router;
